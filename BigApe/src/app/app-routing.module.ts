aa87760729b152c0481b9714b3f1a51fc2c0636f
KayAreBe <kjkoenaite@gmail.com>
2023-06-07 09:26:59 +0200

    first commit

---
+++ BigApe/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LandingComponent } from './pages/landing/landing.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+
+const routes: Routes = [
+{path:'landing', component:LandingComponent},
+{path: 'login', component:LoginComponent},
+{path: 'register', component:RegisterComponent},
+{path: 'home', component:HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
